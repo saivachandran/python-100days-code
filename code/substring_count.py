fb9cc80926e17e8a5a2a1f5bd85657014775f37b
saivachandran <chandransaiva@gmail.com>
2023-08-20 15:15:31 +0530

    substring_count

---
+++ code/substring_count.py
@@ -0,0 +1,7 @@
+main_string = ("hello, hello hwo are you hello?")
+
+sub_string="hello"
+
+count = main_string.count(sub_string)
+
+print(f"The substring {sub_string} appears {count} times in the main string")
